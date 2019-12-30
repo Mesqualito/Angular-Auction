bbfd4a999511ad5f50f045fb89455fad17ef5eb2
Mesqualito <jochen@gebsattel.rocks>
2019-12-30 17:30:50 +0100

    styling the home.component (in progress: Part1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {MatButtonModule} from "@angular/material/button";
@@ -16,7 +15,6 @@ import {ProductService} from "./shared/services";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
