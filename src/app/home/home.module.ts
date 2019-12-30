7fc4e0c1b982528f6b63aff561b9e436955dd3db
Mesqualito <jochen@gebsattel.rocks>
2019-12-30 16:39:46 +0100

    Create each view as feature module (lazy loading), in practice: 1.) generate the module: '$ ng generate module home' 2.) generate the component: '$ ng generate component home'

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule
   ]
