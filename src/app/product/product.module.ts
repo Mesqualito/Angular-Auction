b42392536ebfc1cd1f2b4ce76ab7c6b8bd53096b
Mesqualito <jochen@gebsattel.rocks>
2020-01-20 12:55:55 +0100

    $ nvm install --lts $ npm install -g npm $ npm update -g @angular/cli $ npm update @angular/cli
    $ ng g m product
    $ ng g c product
    $ ng g c product/product-detail
    $ ng g c product/product-suggestion

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {ProductComponent} from './product.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {ProductSuggestionComponent} from './product-suggestion/product-suggestion.component';
+
+
+@NgModule({
+  declarations: [ProductComponent, ProductDetailComponent, ProductSuggestionComponent],
+  imports: [
+    CommonModule,
+    FlexLayoutModule
+  ]
+})
+export class ProductModule { }
